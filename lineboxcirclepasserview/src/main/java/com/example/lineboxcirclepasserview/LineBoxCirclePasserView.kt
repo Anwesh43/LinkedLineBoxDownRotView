d1cd2aa77f551ac3a021b7da9147d8ca314437ca
Anwesh43 <anweshthecool0@gmail.com>
2021-01-11 00:41:13 +0530

    adding LineBoxCirclePasserView class

--- lineboxcirclepasserview/src/main/java/com/example/lineboxcirclepasserview/LineBoxCirclePasserView.kt
+++ lineboxcirclepasserview/src/main/java/com/example/lineboxcirclepasserview/LineBoxCirclePasserView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawLBCPNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBoxCirclePasser(scale, w, h, paint)
 }
+
+class LineBoxCirclePasserView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
