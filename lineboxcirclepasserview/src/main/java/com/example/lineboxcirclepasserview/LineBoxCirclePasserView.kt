5f3b0f5426238e3aee3512550ca4d990bf15fb8e
Anwesh43 <anweshthecool0@gmail.com>
2021-01-11 00:35:24 +0530

    adding canvas extension methods

--- lineboxcirclepasserview/src/main/java/com/example/lineboxcirclepasserview/LineBoxCirclePasserView.kt
+++ lineboxcirclepasserview/src/main/java/com/example/lineboxcirclepasserview/LineBoxCirclePasserView.kt
@@ -27,3 +27,33 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineBoxCirclePasser(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, 0f)
+    drawCircle(0f, -size + h * (1 - sc3), size * sc2, paint)
+    save()
+    translate(-size / 2, size / 2)
+    for (j in 0..1) {
+        save()
+        translate(size * j, 0f)
+        drawLine(0f, -size * sc4, 0f, -size * sc1, paint)
+        restore()
+    }
+    restore()
+    restore()
+}
+
+fun Canvas.drawLBCPNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBoxCirclePasser(scale, w, h, paint)
+}
