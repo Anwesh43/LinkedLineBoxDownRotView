61ba80df00fdc34e0116df0a5dd59b5e7352eb7c
Anwesh43 <anweshthecool0@gmail.com>
2021-01-10 21:08:50 +0530

    adding Canvas extension methods

--- lineboxdownrotview/src/main/java/com/example/lineboxdownrotview/LineBoxDownRotView.kt
+++ lineboxdownrotview/src/main/java/com/example/lineboxdownrotview/LineBoxDownRotView.kt
@@ -29,3 +29,33 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineBoxRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val lSize : Float = Math.min(w, h) / lSizeFactor
+    val rSize : Float = Math.min(w, h) / rSizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, 0f)
+    save()
+    translate(-lSize + (w / 2) * (sc1 - sc4), 0f)
+    drawLine(0f, 0f, lSize, 0f, paint)
+    restore()
+    save()
+    rotate(90f * sc3)
+    translate(0f, (h / 2) * (sc2 + sc4) + (rSize * sc4))
+    drawRect(RectF(-rSize, -rSize, 0f, 0f), paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawLBRDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBoxRot(scale, w, h, paint)
+}
