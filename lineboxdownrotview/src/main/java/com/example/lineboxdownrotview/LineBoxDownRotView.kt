e8cb1bc16e760ab98849dafc2e695d06cb80343a
Anwesh43 <anweshthecool0@gmail.com>
2021-01-10 20:34:53 +0530

    adding libraries and constants

--- lineboxdownrotview/src/main/java/com/example/lineboxdownrotview/LineBoxDownRotView.kt
+++ lineboxdownrotview/src/main/java/com/example/lineboxdownrotview/LineBoxDownRotView.kt
@@ -1,2 +1,28 @@
 package com.example.lineboxdownrotview
 
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val parts : Int = 5
+val scGap : Float = 0.02f
+val strokeFactor : Float = 90f
+val lSizeFactor : Float = 3.2f
+val rSizeFactor : Float = 9.8f
+val delay : Long = 20
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#4CAF50",
+    "#795548",
+    "#FF5722",
+    "#03A9F4"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val backColor : Int = Color.parseColor("#BDBDBD")
+
