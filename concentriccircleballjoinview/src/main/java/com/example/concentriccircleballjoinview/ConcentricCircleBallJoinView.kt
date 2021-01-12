f0f8eaec201060e2c44cdfbd5d43db24673c607f
Anwesh43 <anweshthecool0@gmail.com>
2021-01-13 01:58:12 +0530

    adding Canvas extension methods

--- concentriccircleballjoinview/src/main/java/com/example/concentriccircleballjoinview/ConcentricCircleBallJoinView.kt
+++ concentriccircleballjoinview/src/main/java/com/example/concentriccircleballjoinview/ConcentricCircleBallJoinView.kt
@@ -23,8 +23,36 @@ val colors : Array<Int> = arrayOf(
     Color.parseColor(it)
 }.toTypedArray()
 val backColor : Int = Color.parseColor("#BDBDBD")
+val rFactor : Float = 9.8f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawConcentricBallJoin(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    var sck : Float = 0f
+    val gap : Float = size / parts
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..(parts - 1)) {
+        val r : Float = gap * (j + 1)
+        val scj = scale.divideScale(1, 3).divideScale(j, parts).sinify()
+        drawArc(RectF(-r, -r, r, r), 0f, 360f * scj, false, paint)
+        sck += (r * scj)
+    }
+    val sc1 : Float = scale.divideScale(0, 3)
+    val sc2 : Float = scale.divideScale(2, 3)
+    drawCircle(sck, 0f, (sc1 - sc2) * Math.min(w, h) / rFactor, paint)
+    restore()
+}
+
+fun Canvas.drawCCBJNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawConcentricBallJoin(scale, w, h, paint)
+}
