5651f21862a3622ad768c489ccf7244f5466e783
Anwesh43 <anweshthecool0@gmail.com>
2021-01-12 13:25:28 +0530

    adding DivideBarLineView class

--- dividebarlineview/src/main/java/com/example/dividebarlineview/DivideBarLineView.kt
+++ dividebarlineview/src/main/java/com/example/dividebarlineview/DivideBarLineView.kt
@@ -67,3 +67,19 @@ fun Canvas.drawDBLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawDivideBarLine(scale, w, h, paint)
 }
+
+class DivideBarLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
