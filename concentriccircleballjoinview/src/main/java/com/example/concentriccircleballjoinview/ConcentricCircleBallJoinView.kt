7b80650c0d3c63e0364691f97e5d1705c44c93ef
Anwesh43 <anweshthecool0@gmail.com>
2021-01-13 01:24:49 +0530

    adding constants and libraries

--- concentriccircleballjoinview/src/main/java/com/example/concentriccircleballjoinview/ConcentricCircleBallJoinView.kt
+++ concentriccircleballjoinview/src/main/java/com/example/concentriccircleballjoinview/ConcentricCircleBallJoinView.kt
@@ -1,2 +1,25 @@
 package com.example.concentriccircleballjoinview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+
+val parts : Int = 5
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 2.9f
+val delay : Long = 20
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#009688",
+    "#4CAF50",
+    "#3F51B5",
+    "#FF5722"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val backColor : Int = Color.parseColor("#BDBDBD")
