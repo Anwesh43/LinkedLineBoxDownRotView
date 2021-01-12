2f1477a3c9b14a7c69234d5b867ac36a3f15c1ff
Anwesh43 <anweshthecool0@gmail.com>
2021-01-13 02:47:38 +0530

    adding fixes

--- concentriccircleballjoinview/src/main/java/com/example/concentriccircleballjoinview/ConcentricCircleBallJoinView.kt
+++ concentriccircleballjoinview/src/main/java/com/example/concentriccircleballjoinview/ConcentricCircleBallJoinView.kt
@@ -24,7 +24,7 @@ val colors : Array<Int> = arrayOf(
     Color.parseColor(it)
 }.toTypedArray()
 val backColor : Int = Color.parseColor("#BDBDBD")
-val rFactor : Float = 9.8f
+val rFactor : Float = 14.8f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -37,6 +37,7 @@ fun Canvas.drawConcentricBallJoin(scale : Float, w : Float, h : Float, paint : P
     val gap : Float = size / parts
     save()
     translate(w / 2, h / 2)
+    paint.style = Paint.Style.STROKE
     for (j in 0..(parts - 1)) {
         val r : Float = gap * (j + 1)
         val scj = scale.divideScale(1, 3).divideScale(j, parts).sinify()
@@ -45,6 +46,7 @@ fun Canvas.drawConcentricBallJoin(scale : Float, w : Float, h : Float, paint : P
     }
     val sc1 : Float = scale.divideScale(0, 3)
     val sc2 : Float = scale.divideScale(2, 3)
+    paint.style = Paint.Style.FILL
     drawCircle(sck, 0f, (sc1 - sc2) * Math.min(w, h) / rFactor, paint)
     restore()
 }
@@ -90,6 +92,7 @@ class ConcentricCircleBallJoinView(ctx : Context) : View(ctx) {
         fun startUpdating(cb : () -> Unit) {
             if (dir == 0f) {
                 dir = 1f - 2 * prevScale
+                cb()
             }
         }
     }
