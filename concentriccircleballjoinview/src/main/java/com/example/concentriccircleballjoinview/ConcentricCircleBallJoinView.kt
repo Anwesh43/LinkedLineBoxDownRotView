1a4fa124743b3d7380ff3b28263d40ac7738738d
Anwesh43 <anweshthecool0@gmail.com>
2021-01-13 02:15:11 +0530

    adding CCBJNode class

--- concentriccircleballjoinview/src/main/java/com/example/concentriccircleballjoinview/ConcentricCircleBallJoinView.kt
+++ concentriccircleballjoinview/src/main/java/com/example/concentriccircleballjoinview/ConcentricCircleBallJoinView.kt
@@ -119,4 +119,45 @@ class ConcentricCircleBallJoinView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CCBJNode(var i : Int, val state : State = State()) {
+
+        private var next : CCBJNode? = null
+        private var prev : CCBJNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = CCBJNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCCBJNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CCBJNode {
+            var curr : CCBJNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
