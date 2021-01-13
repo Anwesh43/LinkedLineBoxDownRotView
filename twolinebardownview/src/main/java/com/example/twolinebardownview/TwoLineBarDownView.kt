941b14043741ec42bf95c75f4c811660669e8dd2
Anwesh43 <anweshthecool0@gmail.com>
2021-01-14 04:46:00 +0530

    adding constants and libraries

---
+++ twolinebardownview/src/main/java/com/example/twolinebardownview/TwoLineBarDownView.kt
@@ -0,0 +1,28 @@
+package com.example.twolinebardownview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+
+val parts : Int = 5
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 2.9f
+val barWFactor : Float = 9.8f
+val delay : Long = 20
+val barHFactor : Float = 6f
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#4CAF50",
+    "#795548",
+    "#2196F3",
+    "#FFEB3B"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val backColor : Int = Color.parseColor("#BDBDBD")
