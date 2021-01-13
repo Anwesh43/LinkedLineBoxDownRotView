da9f5543dd6907b34d5b1d7663db4faa0fdf3c11
Anwesh43 <anweshthecool0@gmail.com>
2021-01-14 05:02:05 +0530

    adding Canvas extension methods

--- twolinebardownview/src/main/java/com/example/twolinebardownview/TwoLineBarDownView.kt
+++ twolinebardownview/src/main/java/com/example/twolinebardownview/TwoLineBarDownView.kt
@@ -31,3 +31,36 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawTwoLineBarDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val barW : Float = Math.min(w, h) / barWFactor
+    val barH : Float = size / barHFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2 - barW / 2, h)
+    for (j in 0..1) {
+        save()
+        translate(-barW * j, 0f)
+        drawLine(0f, -size * sc5, 0f, -size * sc1, paint)
+        restore()
+    }
+    save()
+    translate(0f, -(size) + (size - barH) * sc3)
+    drawRect(RectF(barW * sc2, 0f, barW * sc4, barH), paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawTLBDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawTwoLineBarDown(scale, w, h, paint)
+}
