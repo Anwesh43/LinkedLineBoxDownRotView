a8c282ac1c3553f3a67e9dd1091728542c6a0f90
Anwesh43 <anweshthecool0@gmail.com>
2021-01-14 05:12:47 +0530

    adding TLBDNode class

--- twolinebardownview/src/main/java/com/example/twolinebardownview/TwoLineBarDownView.kt
+++ twolinebardownview/src/main/java/com/example/twolinebardownview/TwoLineBarDownView.kt
@@ -127,4 +127,45 @@ class TwoLineBarDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TLBDNode(var i : Int, val state : State = State()) {
+
+        private var next : TLBDNode? = null
+        private var prev : TLBDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = TLBDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTLBDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TLBDNode {
+            var curr : TLBDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
