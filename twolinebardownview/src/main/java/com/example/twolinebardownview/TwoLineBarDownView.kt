0311f1bb5aa7fdbf02255455fe2303422c3b6d1d
Anwesh43 <anweshthecool0@gmail.com>
2021-01-14 05:04:02 +0530

    adding TwoLineBarDownView class

--- twolinebardownview/src/main/java/com/example/twolinebardownview/TwoLineBarDownView.kt
+++ twolinebardownview/src/main/java/com/example/twolinebardownview/TwoLineBarDownView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawTLBDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawTwoLineBarDown(scale, w, h, paint)
 }
+
+class TwoLineBarDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
