0cba1b97ef434ba714c0c8d22065a5cd6f2d0b19
Anwesh43 <anweshthecool0@gmail.com>
2021-01-17 03:28:58 +0530

    adding TwoRotLineBoxCreateView class

--- tworotlineboxcreatorview/src/main/java/com/example/tworotlineboxcreatorview/TwoRotLineBoxCreateView.kt
+++ tworotlineboxcreatorview/src/main/java/com/example/tworotlineboxcreatorview/TwoRotLineBoxCreateView.kt
@@ -67,3 +67,18 @@ fun Canvas.drawTRLBCNode(i : Int, scale : Float, paint : Paint) {
     drawTwoRotLineBoxCreate(scale, w, h, paint)
 }
 
+class TwoRotLineBoxCreateView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
