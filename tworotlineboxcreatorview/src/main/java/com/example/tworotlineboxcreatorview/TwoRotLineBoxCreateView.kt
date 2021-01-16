4135b170bbdeefac78fadcec55a25cb8449c4c96
Anwesh43 <anweshthecool0@gmail.com>
2021-01-17 03:03:29 +0530

    adding constants and libraries

---
+++ tworotlineboxcreatorview/src/main/java/com/example/tworotlineboxcreatorview/TwoRotLineBoxCreateView.kt
@@ -0,0 +1,27 @@
+package com.example.tworotlineboxcreatorview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 2.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#673AB7",
+    "#4CAF50",
+    "#795548",
+    "#03A9F4"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val gapFactor : Float = 8.9f
