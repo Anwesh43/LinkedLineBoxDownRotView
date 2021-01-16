9606587c17cb8ddaf3a823406710ba5c2f4d4708
Anwesh43 <anweshthecool0@gmail.com>
2021-01-17 03:18:29 +0530

    adding Canvas extension merthods

--- tworotlineboxcreatorview/src/main/java/com/example/tworotlineboxcreatorview/TwoRotLineBoxCreateView.kt
+++ tworotlineboxcreatorview/src/main/java/com/example/tworotlineboxcreatorview/TwoRotLineBoxCreateView.kt
@@ -30,3 +30,40 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawTwoRotLineBoxCreate(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val gap : Float = Math.min(w, h) / gapFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float  = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f - 2 * j)
+        save()
+        translate(-size / 2, -gap / 2)
+        rotate(90f * sf2)
+        drawLine(0f, 0f, 0f, -size * sf1, paint)
+        restore()
+        save()
+        translate(- w / 2 + (w / 2 - size / 2) * sf4, 0f)
+        drawRect(RectF(0f, -gap * sf3, size, 0f), paint)
+        restore()
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawTRLBCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawTwoRotLineBoxCreate(scale, w, h, paint)
+}
+
